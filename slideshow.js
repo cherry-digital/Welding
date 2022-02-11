e208b5a147481c3a9717220bc3f89c0f981dd378
cherry-digital <98630255+cherry-digital@users.noreply.github.com>
2022-02-11 11:09:17 +0100

    Add files via upload

---
+++ slideshow.js
@@ -0,0 +1,29 @@
+$('.slideshow-references').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+            }
+    },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+            }
+    },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+            }
+    }
+        ]
+});
